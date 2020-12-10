8916f0d58a8b2ec81ef2cd6c8042e9018a1c186b
Ferz Connect <ferzconnect@Ferzs-MacBook-Air.local>
2020-12-10 14:32:25 -0500

    First commit

---
+++ installjenkins.sh
@@ -0,0 +1,20 @@
+sudo apt-get update
+#update
+
+sudo apt-get install default-jdk -y
+#java install
+
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+#Import the GPG key for Jenkins repo
+
+echo deb http://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
+#Add the repo to list of sources
+
+sudo apt-get update
+#Update package index
+
+sudo apt-get install jenkins
+#Install Jenkins
+
+systemctl status jenkins
+#Shows jenkins status
